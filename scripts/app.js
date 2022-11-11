8afa152c646c7db3c1393e456ff51420c1096fcd
ermaitland <maitland.eleanor@gmail.com>
2022-11-11 10:52:16 +0000

    grid and movement

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,11 @@
 function init() {
   const grid = document.querySelector(".grid");
   const cells = [];
+  let frogPosition = 94;
+  let obj1Position = 70;
 
   const width = 10;
-  const cellCount = width * width - 4;
+  const cellCount = width * width;
 
   function createGrid() {
     for (let i = 0; i < cellCount; i++) {
@@ -15,9 +17,75 @@ function init() {
   }
   createGrid();
 
-  function frogPosition(numberOfCell) {
-    cells[numberOfCell].classList.add("frog");
+  function addFrog(frogPosition) {
+    cells[frogPosition].classList.add("frog");
+    console.log("frog Added to" + frogPosition);
   }
-  frogPosition(91);
+  addFrog(frogPosition);
+
+  function removeFrog(frogPosition) {
+    cells[frogPosition].classList.remove("frog");
+    console.log("frog removed from" + frogPosition);
+  }
+
+  function moveFrog(e) {
+    removeFrog(frogPosition);
+    const x = frogPosition % width;
+    const y = Math.floor(frogPosition / width);
+    switch (e.keyCode) {
+      case 37:
+        if (x > 0) frogPosition--;
+        break;
+      case 38:
+        if (y > 0) frogPosition -= width;
+        break;
+      case 39:
+        if (x < width - 1) frogPosition++;
+        break;
+      case 40:
+        if (y < width - 1) frogPosition += width;
+    }
+    addFrog(frogPosition);
+  }
+
+  function addObj1(position) {
+    cells[position].classList.add("guard");
+  }
+  addObj1(obj1Position);
+
+  function removeObj1(position) {
+    cells[position].classList.remove("guard");
+  }
+  // removeObj1(obj1Position1);
+
+  function guardMovement() {
+    setInterval(() => {
+      if (obj1Position + 1 < 80) {
+        obj1Position++;
+        addObj1(obj1Position);
+        setTimeout(() => {
+          removeObj1(obj1Position - 1);
+        }, 1990);
+      } else if (obj1Position === 79) {
+        obj1Position = 70;
+        addObj1(obj1Position);
+        setTimeout(() => {
+          removeObj1(79);
+        }, 2000);
+      }
+    }, 2000);
+  }
+  guardMovement();
+
+  function collision() {
+    if (frogPosition === obj1Position) {
+      cells[frogPosition].classList.remove("frog");
+      cells[obj1Position].classList.remove("guard");
+      cells[frogPosition].classList.add("collision");
+    }
+  }
+  collision();
+
+  document.addEventListener("keyup", moveFrog);
 }
 window.addEventListener("DOMContentLoaded", init);
